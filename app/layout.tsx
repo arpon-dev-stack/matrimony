ea9ba389f4dc56c23fe394bf2b39a539d3c3ff67
Arpon das <arpon.front@gmail.com>
2026-08-11 00:13:38 +0600

    have to start build first

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,23 +1,19 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+'use client';
+
+// import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { AuthProvider } from "./_store/AuthContext";
 import Note from "@/components/ui/Note";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+// const geistSans = Geist({
+//   variable: "--font-geist-sans",
+//   subsets: ["latin"],
+// });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
-export const metadata: Metadata = {
-  title: "Metrimony",
-  description: "A modern matrimonial web app for meaningful connections.",
-};
+// const geistMono = Geist_Mono({
+//   variable: "--font-geist-mono",
+//   subsets: ["latin"],
+// });
 
 export default function RootLayout({
   children,
@@ -27,7 +23,7 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased scroll-smooth`}
+      className={`h-full antialiased scroll-smooth`}
     >
       <body className="min-h-full flex flex-col">
         <Note/>
